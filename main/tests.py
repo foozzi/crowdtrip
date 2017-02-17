356e977251510cf081934e0331affaf8b3e7372e
foozzi <foozzione@gmail.com>
2017-02-17 17:49:11 +0000

    add tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from myapp.models import Register
 
-# Create your tests here.
+class RegisterTestCase(TestCase):
+	def setUp(self):
+		print 'todo'
+
+	def test_registering(self):
+		print 'todo2'
